56a91ba2abcc4b77e488ba6af3ae1cb3ae1484dd
Aayush <ayushkarkee6@gmail.com>
2023-06-17 16:52:44 +0545

    12 hour formate updated

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 let a;
 let date;
 let time;
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'}
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+// let format;
+
+// if(hour >= 12){
+//     format = "PM"
+// }else {
+//     format = "AM"
+// }
 setInterval (() => {
     a = new Date();
-    date = a.toLocaleDateString(undefined, options);
-    time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+    var hour = a.getHours();
+    var format = hour >= 12 ? "PM" : "AM";
+    hour = hour % 12;
+    date = a.toLocaleDateString("en-US", options);
+    time = hour + ':' + a.getMinutes() + ':' + a.getSeconds() + ' ' + format;
     document.getElementById('time').innerHTML = time + "<br>on " + date ;
 }, 1000);
